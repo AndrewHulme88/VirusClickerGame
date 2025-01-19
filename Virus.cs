226cca450426530772d805ec35a9793d40d72438
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2025-01-19 11:43:24 +1100

    Add files via upload

--- Virus.cs
+++ Virus.cs
@@ -9,6 +9,12 @@ public class Virus : MonoBehaviour
     [SerializeField] int bugsCaughtscore = 1;
     [SerializeField] int health = 1;
 
+    [Header("Splitter")]
+    [SerializeField] bool isSplitter = false;
+    [SerializeField] GameObject splitVirusPrefab;
+    [SerializeField] int splitCount = 2;
+    [SerializeField] float splitOffest = 1f;
+
     private Rigidbody2D rb;
     private Mainframe mainframe;
     private GameManager gameManager;
@@ -38,6 +44,10 @@ public class Virus : MonoBehaviour
         health--;
         if(health <= 0)
         {
+            if(isSplitter)
+            {
+                Split();
+            }
             gameManager.AddBugsCaughtScore(bugsCaughtscore);
             Destroy(this.gameObject);
         }
@@ -50,6 +60,15 @@ public class Virus : MonoBehaviour
             gameManager.AddInfectedScore(infectionScore);
             screenShake.TriggerShake();
             Destroy(this.gameObject);
+        }
+    }
+
+    private void Split()
+    {
+        for(int i = 0; i < splitCount; i++)
+        {
+            Vector3 offset = new Vector3(Random.Range(-splitOffest, splitOffest), Random.Range(-splitOffest, splitOffest), 0);
+            Instantiate(splitVirusPrefab, transform.position + offset, Quaternion.identity);
         }
     }
 }
