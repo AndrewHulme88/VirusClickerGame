d5693bbef92005759926670a8058eb73c03593ae
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2025-01-20 09:09:08 +1100

    Add files via upload

--- CustomCursor.cs
+++ CustomCursor.cs
@@ -8,6 +8,7 @@ public class CustomCursor : MonoBehaviour
     [SerializeField] Texture2D largeCursor;
     [SerializeField] Vector2 cursorHotspot = Vector2.zero;
     [SerializeField] float cursorAnimationDuration = 0.2f;
+    [SerializeField] GameObject particlesPrefab;
 
     void Start()
     {
@@ -24,8 +25,17 @@ public class CustomCursor : MonoBehaviour
 
     private System.Collections.IEnumerator ChangeCursorOnClick()
     {
+        CreateHitParticles();
         Cursor.SetCursor(largeCursor, cursorHotspot, CursorMode.Auto);
         yield return new WaitForSeconds(cursorAnimationDuration);
         Cursor.SetCursor(smallCursor, cursorHotspot, CursorMode.Auto);
+    }
+
+    private void CreateHitParticles()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 1f;
+        GameObject newParticles = Instantiate(particlesPrefab, mousePosition, Quaternion.identity);
+        Destroy(newParticles, 2f);
     }
 }
