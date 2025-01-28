060231c6c1b439a52213544d63b0af4a85c21560
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2025-01-29 08:52:16 +1100

    Add files via upload

--- Score.cs
+++ Score.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public static class Score 
 {
     public static int score = 0;
+
+    public static void SaveHighScore()
+    {
+        int highScore = PlayerPrefs.GetInt("HighScore", 0);
+        if (score > highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            PlayerPrefs.Save();
+        }
+    }
+
+    public static int GetHighScore()
+    {
+        return PlayerPrefs.GetInt("HighScore", 0);
+    }
 }
